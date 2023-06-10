28d308f76cf7bfa485fa7b449413bcca01e7f82b
Gabriel C <gabriel@nvgo.com.br>
2023-06-10 00:46:50 -0300

    fix: fix segurança banco de dados

--- conexao/conexao.php
+++ conexao/conexao.php
@@ -1,14 +1,9 @@
 <?php
-define("config_db_host", "containers-us-west-71.railway.app");
-define("config_db_login", "root");
-define("config_db_senha", "NSuHzbJQLScB5jB0JMTT");
-define("config_db_base", "railway");
 header_remove('set-cookie');
 header("Cache-Control: public, max-age=120");
 
-
 function conectaDb(){
-	$mysqli = new mysqli(constant("config_db_host"), constant("config_db_login"), constant("config_db_senha"), constant("config_db_base"));
+	$mysqli = new mysqli($_SERVER['HOST'], $_SERVER['LOGIN'], $_SERVER['PASSWORD'], $_SERVER['BASE']);
 	if ($mysqli->connect_errno) {
 	    echo "Em manutenção...";
 	    exit;
